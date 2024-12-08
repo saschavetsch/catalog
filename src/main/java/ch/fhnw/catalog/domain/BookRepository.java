5065d7e1bf8c391918d6e0513e52ae9ab9c728c3
nom <noeddi2be@users.noreply.github.com>
2024-12-08 12:25:23 +0100

    @WebMvcTest for controller controller class implemented, added @Repository annotation to book repository. TBD Implement better search: same search term is used for all book attributes atm.

--- src/main/java/ch/fhnw/catalog/domain/BookRepository.java
+++ src/main/java/ch/fhnw/catalog/domain/BookRepository.java
@@ -1,9 +1,11 @@
 package ch.fhnw.catalog.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, String> {
     List<Book> findAllByTitleContainingIgnoreCaseOrAuthorContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String title, String author, String description);
 }
