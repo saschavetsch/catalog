2efc1f70208632c82bf9d808280fb62759991884
nom <noeddi2be@users.noreply.github.com>
2025-01-02 14:01:21 +0100

    Finalized Gatling load testing

--- src/test/java/ch/fhnw/catalog/loadtests/CatalogLoadTests.java
+++ src/test/java/ch/fhnw/catalog/loadtests/CatalogLoadTests.java
@@ -6,6 +6,9 @@ import static io.gatling.javaapi.http.HttpDsl.*;
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
 
+// Run the tests with: mvn gatling:test
+// Order application needs to be running for the tests to work.
+// Application can be started normally with docker-compose up --build.
 public class CatalogLoadTests extends Simulation {
     HttpProtocolBuilder httpProtocol = 
         http.baseUrl("http://localhost:8080")
